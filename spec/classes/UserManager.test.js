3a806eff074926ab1277652982a71800b4f7c28e
Jacob Mather <jmather@jmather.com>
2019-05-02 23:38:59 -0400

    More user manager tests

--- spec/classes/UserManager.test.js
+++ spec/classes/UserManager.test.js
@@ -19,4 +19,12 @@ describe('UserManager', () => {
         userManager._runCleanup();
         expect(userManager.users.test).toBe(undefined);
     });
+
+    it('does not purge current users', () => {
+        const user = new User('test', 'test', 'test');
+        user.last_active = Date.now() - (userManager._cleanupMaxAge - 1);
+        userManager.users.test = user;
+        userManager._runCleanup();
+        expect(userManager.users.test).toBe(user);
+    });
 });
